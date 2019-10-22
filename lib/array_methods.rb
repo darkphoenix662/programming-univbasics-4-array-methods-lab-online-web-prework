f0bbd2b5b4f8a4b09ce0074b9171cb33311000d0
Sean Good <tree662@protonmail.com>
2019-10-22 20:14:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,11 +11,11 @@ def using_reverse(array)
 end
 
 def using_first(array)
-  array.first("wow")
+  array.first
 end
 
 def using_last(array)
-  array.last("arrays!")
+  
 end
 
 def using_size(array)
